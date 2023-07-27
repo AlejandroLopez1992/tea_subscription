fb8445c18f765b9c6de9433666bcc79d5fc0578b
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-07-27 09:50:23 -0600

    Test: Set up customer_subscription create happy path testing

---
+++ spec/requests/customer_subscriptions_spec.rb
@@ -0,0 +1,31 @@
+require 'rspec'
+
+RSpec.describe "CustomerSubscriptions API calls" do
+  describe "POST /api/v1/customer_subscriptions" do
+    it "passed valid customer email and subscription title, is created" do
+      customer = create(:customer)
+      subscription = create(:subscription)
+
+      customer_subscription_params = {
+        "customer_email": customer.email,
+        "subscription_title": subscription.title
+      }
+
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      post "/api/v1/customer_subscriptions", headers: headers, params: JSON.generate(customer_subscription: customer_subscription_params)
+
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
+
+      formatted_responce = JSON.parse(response.body, symbolize_names: true)
+
+      expect(formatted_responce).to be_a(Hash)
+      expect(formatted_responce).to have_key(:message)
+      expect(formatted_responce[:message]).to eq("Succesfully created customer subscription")
+
+      expect(customer.subscriptions).to include(subscription)
+      expect(subscription.customer).to include(customer)
+    end
+  end
+end
