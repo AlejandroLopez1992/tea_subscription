37395c82429feba25723ab8dbe2f1465aa9da047
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-07-27 10:22:23 -0600

    Feat: create find_customer_id and find_subscription_id provate methods within customer_subscriptions_controller.rb

--- app/controllers/api/v1/customer_subscriptions_controller.rb
+++ app/controllers/api/v1/customer_subscriptions_controller.rb
@@ -1,6 +1,26 @@
 class Api::V1::CustomerSubscriptionsController < ApplicationController
 
   def create
-    
+    customer_id = find_customer_id(customer_subscription_params)
+    subscription_id = find_subscription_id(customer_subscription_params)
+    customer_subscription = CustomerSubscription.new(customer_id: customer_id, subscription_id: subscription_id)
+    if customer_subscription.save
+      require 'pry'; binding.pry
+      render json: CustomerSubscriptionSerializer.new(customer_subscription).created, status: 201
+    end
   end
+
+  private
+
+    def customer_subscription_params
+      params.require(:customer_subscription).permit(:customer_email, :subscription_title)
+    end
+
+    def find_customer_id(customer_subscription_params)
+      customer_id = Customer.find_by(email: customer_subscription_params[:customer_email]).id
+    end
+
+    def find_subscription_id(customer_subscription_params)
+      subscription_id = Subscription.find_by(title: customer_subscription_params[:subscription_title]).id
+    end
 end
