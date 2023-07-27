db7f7d805f4b1f00e7edfce5f7e45df9c80999e3
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-07-27 11:05:27 -0600

    Feat: add sad path testing for CustomerSubscription create method

--- app/controllers/api/v1/customer_subscriptions_controller.rb
+++ app/controllers/api/v1/customer_subscriptions_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::CustomerSubscriptionsController < ApplicationController
+rescue_from NoMethodError, with: :not_found
 
   def create
     customer_id = find_customer_id(customer_subscription_params)
@@ -21,5 +22,13 @@ class Api::V1::CustomerSubscriptionsController < ApplicationController
 
     def find_subscription_id(customer_subscription_params)
       subscription_id = Subscription.find_by(title: customer_subscription_params[:subscription_title]).id
+    end
+
+    def not_found(exception)
+      if exception.message.include?("customer_id")
+        render json: ErrorSerializer.new(exception).customer_not_found, status: 404
+      else
+        render json: ErrorSerializer.new(exception).subscription_not_found, status: 404
+      end
     end
 end
